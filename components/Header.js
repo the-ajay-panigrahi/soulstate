ca7e3c9a15545ecc7d6671260e878ef8c4d225d8
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-06-05 22:33:30 +0530

    feat: add Calendar section

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import { Fugaz_One } from 'next/font/google'
+import Link from 'next/link'
 import React from 'react'
 
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] })
@@ -6,7 +7,7 @@ const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] })
 export default function Header() {
     return (
         <header className='p-4 sm:p-8 flex items-center justify-between gap-4'>
-            <h1 className={"text-base sm:text-lg textGradient " + fugaz.className}>Soul State</h1>
+            <Link href={"/"}><h1 className={"text-base sm:text-lg textGradient " + fugaz.className}>Soul State</h1></Link>
             <div className='flex items-center justify-between'>PLACEHOLDER</div>
         </header>
     )
